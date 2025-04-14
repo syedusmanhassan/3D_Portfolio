fa4d5f7e8e70077d47e60d4ef160154d65115250
syedusmanhassan <usmanhasanwasti@gmail.com>
2025-04-15 00:02:42 +0500

    added navbar and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "./Components/Sections/Hero";
 import ShowcaseSection from "./Components/Sections/ShowcaseSection";
+import Navbar from "./Components/Navbar";
+import LogoSection from "./Components/LogoSection";
 
 const App =()=>{
     return(
     <>
+    <Navbar/>
     <Hero/>
     <ShowcaseSection/>
+    <LogoSection/>
     </>
     );
 }
