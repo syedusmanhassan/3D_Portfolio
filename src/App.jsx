6efe00049a494ed8d38fc0540e0f8039c32498b5
syedusmanhassan <usmanhasanwasti@gmail.com>
2025-04-15 00:42:50 +0500

    Issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "./Components/Sections/Hero";
 import ShowcaseSection from "./Components/Sections/ShowcaseSection";
-
 import LogoSection from "./Components/Sections/LogoSection";
 import FeaturesCards from "./Components/Sections/FeaturesCards";
 import Navbar from "./Components/NavBar";
